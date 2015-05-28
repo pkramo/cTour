4b2aa6464dc9efbde51cba789e70d0a7d2df0016
Bart de Lange <bart.de.lange32@gmail.com>
2015-05-28 11:41:42 +0200

    SphereActivity bijgewerkt en de methods toegevoegd

---
+++ app/src/main/java/nl/bart_de_lange/android/ctour/MapFragment.java
@@ -0,0 +1,20 @@
+package nl.bart_de_lange.android.ctour;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Intent photoActivityIntent = new Intent(getActivity(), SphereActivity.class);
+        startActivity(photoActivityIntent);
+
+        //Inflate the layout
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
--- app/src/main/java/nl/bart_de_lange/android/ctour/SphereActivity.java
+++ app/src/main/java/nl/bart_de_lange/android/ctour/SphereActivity.java
@@ -1,6 +1,73 @@
 package nl.bart_de_lange.android.ctour;
 
-public class SphereActivity {
-    public String Tag = "SphereActivity";
-    public String Pino = "Yolo1";
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.panorama.Panorama;
+import com.google.android.gms.panorama.PanoramaApi.PanoramaResult;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SphereActivity extends Activity implements ConnectionCallbacks,
+        OnConnectionFailedListener {
+
+    public static final String TAG = SphereActivity.class.getSimpleName();
+    private GoogleApiClient mClient;
+    private int PhotoSphere;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mClient = new GoogleApiClient.Builder(this, this, this)
+                .addApi(Panorama.API)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + PhotoSphere);
+        Panorama.PanoramaApi.loadPanoramaInfo(mClient, uri).setResultCallback(new ResultCallback<PanoramaResult>() {
+            @Override
+            public void onResult(PanoramaResult result) {
+                if (result.getStatus().isSuccess()) {
+                    Intent viewerIntent = result.getViewerIntent();
+                    Log.i(TAG, "found viewerIntent: " + viewerIntent);
+                    if (viewerIntent != null) {
+                        startActivity(viewerIntent);
+                    }
+                } else {
+                    Log.e(TAG, "error: " + result);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "connection suspended: " + cause);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult status) {
+        Log.e(TAG, "connection failed: " + status);
+        // TODO fill in
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mClient.disconnect();
+    }
+
 }
